6f8b793b6e02222484363026ed7e0dfaa29db969
George Varghese M <georgevarghese185@yahoo.com>
2016-10-15 12:47:08 +0530

    Updated navigation drawer
    Removed menu items and replaced with "Music Library" and "Settings" (Altered MainActivity.java and activity_main_drawer.xml)

--- app/src/main/java/teefourteen/distroplayer/MainActivity.java
+++ app/src/main/java/teefourteen/distroplayer/MainActivity.java
@@ -83,17 +83,9 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            mainFragment.switchTo(libraryFragment,libraryTag);
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_library) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_settings) {
 
         }
 
