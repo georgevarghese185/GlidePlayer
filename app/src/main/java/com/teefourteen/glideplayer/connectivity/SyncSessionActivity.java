0080b5a345d74cd7232d289234cbc72a1494d62c
George Varghese M <georgevarghese185@yahoo.com>
2017-04-13 21:22:57 +0530

    Minor sync UI fixes, disabled ACRA
    activity_sync_player.xml:
      -Changed top text's color to white
      -Updated frame layout's constraints to be below the top text
    
    activity_sync_session.xml:
      -Renamed to activity_sync_sessions
      -Added appBarLayout around the toolbar
      -Redid some id's
      -fixed constraints of the listView
      -Changed FAB's + color to white
    
    fragment_play_queue.xml:
      -Added toolbar inside appbarlayout
    
    play_queue_track.xml:
      -deleted since it's mostly the same as track.xml. Will reintroduce it when the layout is figured out
    
    sync_session.xml:
      -Changed session_current's text to "Now Playing"
    
    AndroidManifest.xml:
      -Removed application reference to MyApplication to disable ACRA since it's not done. Will link it when ACRA is fully implemented.

--- app/src/main/java/com/teefourteen/glideplayer/connectivity/SyncSessionActivity.java
+++ app/src/main/java/com/teefourteen/glideplayer/connectivity/SyncSessionActivity.java
@@ -14,11 +14,11 @@ public class SyncSessionActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sync_session);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setContentView(R.layout.activity_sync_sessions);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.sync_sessions_toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.sync_create_fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
